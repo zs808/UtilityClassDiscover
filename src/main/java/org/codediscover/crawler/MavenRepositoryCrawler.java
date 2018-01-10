bf9d391f35b072ee2e6f8dea476584782e65e10d
张帅 <565786100@qq.com>
2018-01-10 18:47:52 +0800

    添加POM文件

---
+++ src/main/java/org/codediscover/crawler/MavenRepositoryCrawler.java
@@ -0,0 +1,9 @@
+package org.codediscover.crawler;
+
+/**
+ * Maven仓库爬虫
+ * @author ZhangShuai　at 2018-01-10 16:06
+ * @version 1.0
+ */
+public class MavenRepositoryCrawler {
+}
---
+++ src/main/java/org/codediscover/crawler/bean/MavenPackageInfo.java
@@ -0,0 +1,8 @@
+package org.codediscover.crawler.bean;
+
+/**
+ * @author ZhangShuai　at 2018-01-10 16:08
+ * @version 1.0
+ */
+public class MavenPackageInfo {
+}
